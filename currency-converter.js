ee46bf6ae8bd7154a309f35e58e0f2ac7e7779da
jurakulis21 <71762114+jurakulis21@users.noreply.github.com>
2021-06-05 22:05:50 +0200

    space

--- currency-converter.js
+++ currency-converter.js
@@ -2,14 +2,11 @@ let select = document.querySelectorAll('.select');
 let btn = document.getElementById('button');
 let oputput = document.getElementById('output');
 
-
 fetch('https://api.exchangerate.host/latest?fbclid=IwAR37ezqZVImhicqIFpnxj-y-wP0owKSUoX18MGCkhn6F8vPJ1iB_jAcwY-w')
     .then(data => data.json())
     .then(data => {
         insertValutesInSelect(data);
     })
-
-
 
 function insertValutesInSelect(_data) {
     const entries = Object.keys(_data.rates);
@@ -19,14 +16,12 @@ function insertValutesInSelect(_data) {
     }
 }
 
-
 btn.addEventListener('click', function() {
     let amount = document.getElementById('amount').value;
     const currency1 = select[0].value;
     const currency2 = select[1].value;
     convert(currency1, currency2, amount);
 })
-
 
 function convert(currency1, currency2, amount) {
     fetch('https://api.exchangerate.host/latest?fbclid=IwAR37ezqZVImhicqIFpnxj-y-wP0owKSUoX18MGCkhn6F8vPJ1iB_jAcwY-w')
